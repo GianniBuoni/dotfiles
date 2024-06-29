65cba215035a869103ccfe4cb9af81721d277352
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-06-29 12:16:01 -0400

    Feat: Added suspend option to power menu

--- home/desktops/rofi/power-menu.nix
+++ home/desktops/rofi/power-menu.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: let
   power-menu = pkgs.writeShellScriptBin "power-menu" ''
-    chosen=$(printf "󰌾 Lock Screen\n󰗽 Log Off\n󰜉 Restart\n󰐥 Shutdown" | rofi -dmenu -p "Power Menu " -theme-str "listview {lines:4;}")
+    chosen=$(printf "󰌾 Lock Screen\n󰏤 Suspend\n󰗽 Log Off\n󰜉 Restart\n󰐥 Shutdown" | rofi -dmenu -p "Power Menu " -theme-str "listview {lines:5;}")
 
     case "$chosen" in
       "󰌾 Lock Screen") hyprlock ;;
+      "󰏤 Suspend") systemctl suspend ;;
       "󰗽 Log Off") hyprctl dispatch exit ;;
       "󰜉 Restart") reboot ;;
       "󰐥 Shutdown") poweroff ;;
