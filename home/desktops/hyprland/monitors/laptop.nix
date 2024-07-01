441435e5215a2e747cfe7f04d3c902b6fb8da7ee
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-07-01 14:01:09 -0400

    Hyprland now targets monitor names. Somehow the update fixed an issus witn Krita

--- home/desktops/hyprland/monitors/laptop.nix
+++ home/desktops/hyprland/monitors/laptop.nix
@@ -4,9 +4,10 @@
   ];
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-2,2880x1800@120.0,4320x0,2.0"
-      "DP-1,3840x2160@60.0,0x0,2.0"
-      "DP-3,3840x2160@60.0,1920x0,1.6"
+      "desc:Samsung Display Corp. ATNA40CU05-0,2880x1800@120.0,4320x0,2.0"
+      "desc:HAT Kamvas Pro 19 0xF0000005,3840x2160@60.0,0x0,2.0"
+      "desc:LG Electronics LG HDR 4K 0x00039027,3840x2160@60.0,1920x0,1.6"
+      ",preferred,auto-right,2.0"
     ];
 
     bindl = [
--- home/desktops/hyprland/monitors/shell-scripts/clamshell-mode.nix
+++ home/desktops/hyprland/monitors/shell-scripts/clamshell-mode.nix
@@ -6,9 +6,9 @@
     then
       if [[ $1 == "open" ]]
       then
-        hyprctl keyword monitor "eDP-2,2880x1800@120.0,4320x0,2.0" && hyprctl reload
+        hyprctl keyword monitor "desc:Samsung Display Corp. ATNA40CU05-0,2880x1800@120.0,4320x0,2.0" && hyprctl reload
       else
-        hyprctl keyword monitor "eDP-2,disable"
+        hyprctl keyword monitor "desc:Samsung Display Corp. ATNA40CU05-0,disable"
       fi
     fi
   '';
