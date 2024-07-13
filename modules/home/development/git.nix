9fa83a93e448a48c6296b99634c65cf893c17eb0
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-07-12 21:45:46 -0400

    Added GitHub CLI to home-manager

--- modules/home/development/git.nix
+++ modules/home/development/git.nix
@@ -1,15 +1,14 @@
 {userSettings, ...}: {
-  programs.gh.gitCredentialHelper.enable = true;
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper.enable = true;
+  };
   programs.git = {
     enable = true;
     userName = userSettings.gitUser;
     userEmail = userSettings.gitEmail;
     extraConfig = {
       init.defaultBranch = "main";
-      credential = {
-        "https://github.com".helper = "!gh auth git-credential";
-        "https://gist.github.com".helper = "!gh auth git-credential";
-      };
     };
     ignores = [
       ".DS_Store"
