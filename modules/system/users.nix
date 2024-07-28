3b7b17038a5aa13f8b7460be82a207df1e250de7
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-07-27 21:13:53 -0400

    Configured Auto Login

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -21,8 +21,14 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Display Manager
-  services.xserver.displayManager.gdm.enable = true;
+  services = {
+    # Display Manager
+    xserver.displayManager.gdm.enable = true;
+
+    # Enable automatic login for the user.
+    displayManager.autoLogin.enable = true;
+    displayManager.autoLogin.user = userSettings.userName;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userSettings.userName} = {
