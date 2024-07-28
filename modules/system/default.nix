d188e0848261917f9610d0176146c36a91384abe
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-07-27 21:26:39 -0400

    Going to try no Gnome on handheld

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,19 +1,9 @@
-{systemSettings, ...}: {
-  imports =
-    [
-      ./main.nix
-      ./grub-boot.nix
-      ./programs.nix
-      ./users.nix
-    ]
-    ++ (
-      if (systemSettings.formFactor == "handheld")
-      then [
-        ./gnome.nix
-        ./gnome-extensions.nix
-      ]
-      else [
-        ./hyprland.nix
-      ]
-    );
+{
+  imports = [
+    ./main.nix
+    ./grub-boot.nix
+    ./programs.nix
+    ./users.nix
+    ./hyprland.nix
+  ];
 }
