f6a334cddc1f1cd114c93d358413bdb166a77f7a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-07-27 21:22:01 -0400

    Added more Nvidia Settings

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,4 +1,10 @@
 {
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+  };
 }
