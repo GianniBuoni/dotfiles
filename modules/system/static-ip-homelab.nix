b894cc0d71e095c5ae1c0293e9eb6120cf1fa6af
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-08-20 15:41:19 -0400

    Homelab network fix for Home Assistant

--- modules/system/static-ip-homelab.nix
+++ modules/system/static-ip-homelab.nix
@@ -1,8 +1,11 @@
 {
-  networking.interfaces.enp0s13f0u3u4.ipv4.addresses = [
-    {
-      address = "192.168.1.2";
-      prefixLength = 24;
-    }
-  ];
+  networking = {
+    interfaces.enp0s13f0u3u4.ipv4.addresses = [
+      {
+        address = "192.168.1.2";
+        prefixLength = 24;
+      }
+    ];
+    firewall.allowedTCPPorts = [8123]; # Needed for Home Assistant to work in host mode
+  };
 }
