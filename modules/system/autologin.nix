9096a72e00feb33be61e4e6c8e848b163c6a5b46
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-09-01 11:02:38 -0400

    ⭐ FINALLY GOT SDDM WORKING

--- modules/system/autologin.nix
+++ modules/system/autologin.nix
@@ -1,10 +1,26 @@
 {userSettings, ...}: {
   services = {
-    # Display Manager
-    xserver.displayManager.gdm.enable = true;
+    displayManager = {
+      # Display Manager
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+        # inherit (userSettings) theme;
+      };
 
-    # Enable automatic login for the user.
-    displayManager.autoLogin.enable = true;
-    displayManager.autoLogin.user = userSettings.userName;
+      # Enable automatic login for the user.
+      autoLogin.enable = true;
+      autoLogin.user = userSettings.userName;
+    };
   };
+  # environment.systemPackages = [
+  #   (
+  #     pkgs.catppuccin-sddm.override {
+  #       flavor = "mocha";
+  #       # font = userSettings.nerdFont;
+  #       # background = "../../themes/${userSettings.theme}/wallpaper.jpg";
+  #       # loginBackground = true;
+  #     }
+  #   )
+  # ];
 }
