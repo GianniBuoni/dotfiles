a7de47957a76176d222f236dc35195284e84c9d6
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-09-16 09:19:11 -0400

    Changes to ssdm default session and added new homlab script

--- modules/home/shell-scripts/homelab.nix
+++ modules/home/shell-scripts/homelab.nix
@@ -5,28 +5,24 @@
 }: let
   homelab-up = pkgs.writeShellScriptBin "homelab-up" ''
     cd ~/dotfiles && git add . -Nf && sudo nixos-rebuild switch --flake . && git reset &&
+    cd ~/repos/${userSettings.website} && dcu runtime &&
     cd ~/docker/core-services && dcu &&
-    dc ~/docker/iot-stack && dcu &&
+    cd ~/docker/iot-stack && dcu &&
     cd ~/docker/sync-stack && dcu &&
     cd ~/docker/media-stack && dcu &&
-    cd ~/docker/arr-stack && dcu &&
-    cd ~/repos/${userSettings.website} && dcu runtime
+    cd ~/docker/arr-stack && dcu
+  '';
+  homelab-down = pkgs.writeShellScriptBin "homelab-down" ''
+    cd ~/repos/${userSettings.website} && dcd runtime &&
+    cd ~/docker/core-services && dcd &&
+    cd ~/docker/iot-stack && dcd &&
+    cd ~/docker/sync-stack && dcd &&
+    cd ~/docker/media-stack && dcd &&
+    cd ~/docker/arr-stack && dcd
   '';
 in {
   home.packages = [
     homelab-up
+    homelab-down
   ];
 }
-/*
-WIP
- HOMELAB_UP=$(
- )
- while true; do
- read -p "Have you mounted the external drive?" yn
- case $yn in
-     [Yy]* ) $HOMELAB_UP ;;
-     [Nn]* ) exit ;;
-     * ) echo "Please answer yes or no." ;;
- esac
-*/
-
--- modules/system/plasma.nix
+++ modules/system/plasma.nix
@@ -6,13 +6,13 @@
       sddm = {
         enable = true;
         wayland.enable = true;
-        # inherit (userSettings) theme;
       };
       autoLogin = {
         # Enable automatic login for the user.
         enable = true;
         user = userSettings.userName;
       };
+      defaultSession = "hyprland";
     };
   };
 }
