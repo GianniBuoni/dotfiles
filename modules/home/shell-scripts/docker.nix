dde4663955e0d99f57a8be15bc22a1b7944e3144
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-10-08 19:57:50 -0400

    🐳 Modified bash script for docker compose
    - No longer need sudo since the creation of docker user group

--- modules/home/shell-scripts/docker.nix
+++ modules/home/shell-scripts/docker.nix
@@ -1,16 +1,16 @@
 {pkgs, ...}: let
   dcu = pkgs.writeShellScriptBin "dcu" ''
     if [ $# -eq 0 ]; then
-      sudo docker compose up -d
+      docker compose up -d
     else
-      sudo docker compose --profile $1 up -d
+      docker compose --profile $1 up -d
     fi
   '';
   dcd = pkgs.writeShellScriptBin "dcd" ''
     if [ $# -eq 0 ]; then
-      sudo docker compose down
+      docker compose down
     else
-      sudo docker compose --profile $1 down
+      docker compose --profile $1 down
     fi
   '';
 in {
