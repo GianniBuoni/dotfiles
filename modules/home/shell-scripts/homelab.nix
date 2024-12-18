ca5ee2becb37be9877c5099bca2f5c3017df6c23
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-12-18 11:39:52 -0500

    update homelab-up shell script

--- modules/home/shell-scripts/homelab.nix
+++ modules/home/shell-scripts/homelab.nix
@@ -6,7 +6,7 @@
   homelab-up = pkgs.writeShellScriptBin "homelab-up" ''
     cd ~/dotfiles && git add . -Nf && sudo nixos-rebuild switch --flake . && git reset &&
     cd ~/docker && docker compose up -d &&
-    cd ~/docker/${userSettings.website} && docker compose --profile runtime up
+    cd ~/docker/${userSettings.website} && docker compose --profile runtime up -d
   '';
   homelab-down = pkgs.writeShellScriptBin "homelab-down" ''
     cd ~/docker && docker compose down &&
