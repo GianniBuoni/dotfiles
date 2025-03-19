07f4debaa97d72f7892aa014c7f975c3ad6d9594
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-19 14:24:02 -0400

    add programs for hm
    - aseprite & ldtk for game dev
    - xdg session variables for hyprland

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -10,6 +10,9 @@
     sessionVariables = {
       WLR_NO_HARDWARE_CURSORS = 1; # If the cursor becomes invisible
       NIXOS_OZONE_WL = 1; # Hint electron apps to use Wayland
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_SESSION_DESKTOP = "Hyprland";
     };
 
     systemPackages = with pkgs; [
