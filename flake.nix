22dde82f2c63ad01e8ff737814de2dec8775f841
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-05-31 15:26:08 -0400

    add zen browser

--- flake.nix
+++ flake.nix
@@ -21,6 +21,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
     # STYLING AND UI
     stylix.url = "github:danth/stylix";
   };
--- modules/home/productivity/firefox.nix
+++ modules/home/productivity/firefox.nix
@@ -1,6 +1,17 @@
-{
+{inputs, ...}: {
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+
   programs = {
     firefox.enable = true;
     chromium.enable = true;
+    zen-browser = {
+      enable = true;
+      policies = {
+        DisableAppUpdate = true;
+        DisableTelemetry = true;
+      };
+    };
   };
 }
