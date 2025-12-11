0d56efe14eec65c34784111c7d1fbdc90ec71987
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-12-11 11:59:52 -0500

    feat: add emoji picker command to hyprland

--- modules/features/hyprland/programs.nix
+++ modules/features/hyprland/programs.nix
@@ -7,18 +7,18 @@
       "$fileManager" = "ghostty -e yazi";
       "$menu" = "fuzzel";
       "$web" = "zen-beta";
+      "$emoji" = "bemoji -t";
 
       # program key bindings
       bind = [
         "$mod, T, exec, $terminal" # [T]erminal
         "$mod, E, exec, $fileManager" # [E]xplorer
         "$mod, F, exec, $web" # [F]irefox
-        "$mod, W, killactive"
         "$mod, O, exec, $notes" # [O]bsidian
+        "$mod, W, killactive"
         "$mod, V, togglefloating"
         "$mod, SPACE, exec, $menu"
-        "ALT, SPACE, exec, $run"
-        "SHIFT, SPACE, exec, $emoji"
+        "ALT, SPACE, exec, $emoji"
       ];
 
       exec-once = [
