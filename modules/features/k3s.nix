307e74fae7bd1cd0dc27c0e496f95f5e6d869ad7
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-12-12 13:21:30 -0500

    chore: update secrets

--- modules/features/k3s.nix
+++ modules/features/k3s.nix
@@ -1,5 +1,5 @@
 let
-  tokenPath = "clusterToken";
+  tokenPath = "cluster-secrets/prod/token";
 in {
   flake.aspects.k3s = {
     # base k3s clusetr config
@@ -14,15 +14,18 @@ in {
         extraFlags = toString ["--disable-helm-controller"];
       };
     };
+
     _ = {
       # test-host is a single node custer
       singleNode.nixos.services.k3s.role = "server";
 
       # common multi-node cluster configs
-      multiNode.nixos = {config, ...}: {
+      multiNode.nixos = {config, ...}: let
+        inherit (config.users.users) k3s-user;
+      in {
         sops.secrets.${tokenPath} = {
-          owner = config.users.users.k3s-user.name;
-          inherit (config.users.users.k3s-user) group;
+          owner = k3s-user.name;
+          inherit (k3s-user) group;
         };
 
         services.k3s.tokenFile = config.sops.secrets.${tokenPath}.path;
