3f03d785b4bd923d19b7f4e3a4b3ac1eb9ff81c8
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-01-14 10:35:30 -0500

    refactor: change which secrets are available to duck-muscles
    - chore: update secrets

--- modules/feature-collections/secrets.nix
+++ modules/feature-collections/secrets.nix
@@ -1,13 +1,15 @@
 {lib, ...}: {
   flake.aspects = {aspects, ...}: let
-    clusters = ["testing" "staging" "prod"];
+    clusters = ["testing" "staging"];
 
-    mkAge = name: aspects.sops._.keyPaths "cluster-secrets/${name}/age.agekey";
+    mkTlsCrt = name: aspects.sops._.keyPaths "cluster-secrets/${name}/tls.crt";
+    mkTlsKey = name: aspects.sops._.keyPaths "cluster-secrets/${name}/tls.key";
   in {
     secrets = {
       description = "Collection of secrets for dev machine to access.";
       includes =
-        lib.map mkAge clusters
+        lib.map mkTlsCrt clusters
+        ++ lib.map mkTlsKey clusters
         ++ [(aspects.sops._.keyPaths "luks.key")];
     };
   };
