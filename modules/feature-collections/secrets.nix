1794ce0ab43fd3263836d45b1f127da1165514ec
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-12 13:01:19 -0500

    chore: update secrets
    refactor: add cluster sops secrets back into the system

--- modules/feature-collections/secrets.nix
+++ modules/feature-collections/secrets.nix
@@ -2,12 +2,14 @@
   flake.aspects = {aspects, ...}: let
     clusters = ["dev" "prod"];
     mkTls = name: ext: aspects.sops._.clusters "${name}" "tls.${ext}";
+    mkAge = name: aspects.sops._.clusters "${name}" "age.agekey";
   in {
     secrets = {
       description = "Collection of secrets for dev machine to access.";
       includes =
         lib.forEach clusters (cluster: mkTls cluster "crt")
         ++ lib.forEach clusters (cluster: mkTls cluster "key")
+        ++ lib.forEach clusters mkAge
         ++ [(aspects.sops._.keyPaths "luks.key")];
     };
   };
